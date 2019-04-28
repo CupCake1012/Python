5892bf3215dc87bbe294ab48662401e7232fc858
李悦 <50064951+CupCake1012@users.noreply.github.com>
2019-04-28 12:11:11 +0800

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,15 @@
+s = range(0,50)
+print(range(0,50))
+print(s)
+a=list(s)
+print(a)
+print(s)
+n = 100
+while n>20:
+    n = n - 1
+    if n%2 == 0:
+        continue
+    else:
+        print(n)
+
+
